4cf8b357791f4a6bcd5fb7723903ff673b288971
Dave Wasmer <davewasmer@gmail.com>
2018-02-02 18:01:30 -0700

    WIP

--- app/router.js
+++ app/router.js
@@ -8,20 +8,22 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
 
-  this.route('marketing', { path: '/' }, function() {
-    this.route('why-denali', { resetNamespace: true });
-    this.route('community', { resetNamespace: true });
-    this.route('roadmap', { resetNamespace: true });
-    this.route('addons', { resetNamespace: true }, function() {});
-  });
-
+  this.route('features', { resetNamespace: true });
+  this.route('community', { resetNamespace: true });
+  this.route('addons', { resetNamespace: true }, function() {});
   this.route('docs', { path: '/docs/:version' }, function() {
+    this.route('quickstart');
+    this.route('tutorial', function() {
+    });
     this.route('guides', function() {
       this.route('guide', { path: '/*slug' });
     });
     this.route('apis', { path: '/api' }, function() {
       this.route('api', { path: '/*slug' });
     });
+  });
+  this.route('blog', function() {
+
   });
 
 });
