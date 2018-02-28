263958479ff57d9f7449609da61e7fdefc111a73
Dave Wasmer <davewasmer@gmail.com>
2018-02-28 15:50:29 -0700

    switch version lookup to query params

--- app/pods/docs/route.js
+++ app/pods/docs/route.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model(params) {
-    return this.store.find('version', `@denali-js:core@${ params.version_id }`).then((version) => {
+    return this.store.queryRecord('version', { addon: '@denali-js/core', version: params.version_id }).then((version) => {
       return version.get('doc').then((doc) => {
         return { version, doc };
       });
