a1778f4482fb6c8babb31e8178b147b25ea9cd03
Ilya Radchenko <ilya@burstcreations.com>
2018-03-15 13:08:51 -0400

    chore: fix lint

--- mirage/serializers/version.js
+++ mirage/serializers/version.js
@@ -1,6 +1,7 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
+  // eslint-disable-next-line ember/avoid-leaking-state-in-ember-objects
   include: [ 'addon' ],
   serializeIds: 'always',
 });
